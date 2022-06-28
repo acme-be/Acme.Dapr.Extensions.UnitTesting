58d55880fc86f93f6952bb59abb5a5d64f1ab577
Simon Baudart <simon@contact-me.be>
2022-06-28 14:21:23 +0200

    Create the project

---
+++ src/Acme.Dapr.Extensions.UnitTesting/SetupExtensions.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "TestsExtensions.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Dapr.Client;
+using Moq;
+
+namespace Acme.Dapr.Extensions.UnitTesting;
+
+public static class SetupExtensions
+{
+    public static void SetupGetStateAsync<T>(this Mock<DaprClient> mock, string store, string key, T data)
+    {
+        mock.Setup(x => x.GetStateAsync<T>(store, key, null, null, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(data);
+    }
+}
---
+++ src/Acme.Dapr.Extensions.UnitTesting/VerifiyExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Dapr.Client;
+using Moq;
+
+namespace Acme.Dapr.Extensions.UnitTesting;
+
+public static class VerifiyExtensions
+{
+    public static void VerifyPublishEvent<TExpected>(this Mock<DaprClient> mock, string expectedPubsub, string expectedTopic)
+    {
+        mock.Verify(x => x.PublishEventAsync(expectedPubsub, expectedTopic, It.IsAny<TExpected>(), default), Times.Once);
+    }
+
+    public static void VerifySaveState<TExpected>(this Mock<DaprClient> mock, string expectedStore)
+    {
+        mock.Verify(x => x.SaveStateAsync(expectedStore, It.IsAny<string>(), It.IsAny<TExpected>(), It.IsAny<StateOptions>(), It.IsAny<IReadOnlyDictionary<string, string>>(), default),
+            Times.Once);
+    }
+}
